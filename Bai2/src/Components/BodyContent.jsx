28311214dec714f137b22624230502404e6720d1
ThanhNhut79 <thanhnhutvn77@gmail.com>
2023-11-20 23:13:00 +0700

    first commit

--- Bai2/src/Components/BodyContent.jsx
+++ Bai2/src/Components/BodyContent.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import linkedinLogo from "/linkedin.png";
+import facebookLogo from "/facebook.png";
+import instagramLogo from "/instagram.png";
+import twitterLogo from "/twitter.png";
+import buttonIcon from "/icons-next.png";
 
 function BodyContent() {
   return (
@@ -12,10 +17,21 @@ function BodyContent() {
         </div>
         <div className="content-2">
           <label>Email Address</label>
-          <input type="text" />
+          <div className="text-input">
+            <input type="text" />
+            <button className="button-next-icon">
+              <img src={buttonIcon} alt="" />
+            </button>
+          </div>
         </div>
         <div className="content-3">
           <span>Follow us on Social</span>
+          <div className="logo-contact">
+            <img src={twitterLogo} alt="" />
+            <img src={facebookLogo} alt="" />
+            <img src={instagramLogo} alt="" />
+            <img src={linkedinLogo} alt="" />
+          </div>
         </div>
       </div>
     </div>
